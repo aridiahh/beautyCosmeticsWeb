b386c91b1e978ea716bb74d356b51ee951c32718
aridiahlestari <oiyayakkrid@gmail.com>
2023-11-02 17:23:19 +0800

    membuat frontend dan menambah html dan css ke laravel website

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,42 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/product', function () {
+    return view('product');
+});
+
+Route::get('/thankyou', function () {
+    return view('thankyou');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/testimonials', function () {
+    return view('testimonials');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
 });
